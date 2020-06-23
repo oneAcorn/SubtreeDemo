1e63141b671005e08aa6ab07f07e0d12b4666544
sufei <oneacorn@163.com>
2020-06-23 15:35:35 +0800

    implementation subtreeLibrary

--- app/src/main/java/com/acorn/subtreedemo/MainActivity.kt
+++ app/src/main/java/com/acorn/subtreedemo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.acorn.subtreedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.acorn.commonmodule.TestUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val testUtil = TestUtil()
+        testUtil.abc()
     }
 }
